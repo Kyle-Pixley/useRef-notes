8df8b9b0073d0c049caa8d2712416b7d83085bf6
KylePixley <kyle_pixley@hotmail.com>
2023-07-18 16:48:49 -0400

    useRefNotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import './App.css'
+import { render } from 'react-dom'
 
 function App() {
 
   const [ count, setCount ] = useState(0)
+  const [ name, setName ] = useState("")
 
   const incCount = () => {
     setCount(prevCount => prevCount + 1)
@@ -15,14 +17,27 @@ function App() {
 
   // Count renders with useEffect
 
-  const [ renders, setRenders ] = useState(0)
+  // const [ renders, setRenders ] = useState(0)
+  const renderCount = useRef(0)
 
   // useEffect(() => {
   //   setRenders(prevRenders => prevRenders + 1)
-  //   console.log(renders)
   // })
 
   // ! Initial render triggers useEffect, triggers count, triggers useEffect... (infinite loop)
+
+  /* 
+    ? useRef
+    * allows us to keep track of state without trigerring a re-render
+    * an object with .current property
+    * .current persists throughout the full component lifetime
+    * .current is also mutable
+  */
+
+  useEffect(() => {
+    renderCount.current += 1
+  })
+
 
   return (
     <div className='main-container'>
@@ -32,7 +47,11 @@ function App() {
         <button onClick={decCount}>-</button>
       </div>
       <div>
-        <h1>This page re-rendered x times</h1>
+        <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder="Enter your Name" />
+        <p>My name is {name}</p>
+      </div>
+      <div>
+        <h1>This page re-rendered {renderCount.current} times</h1>
       </div>
     </div>
   )
